9b33a30f6335dc10acdbc209d9b9ef1de0b3d712
cleiton97 <146895519+cleiton97@users.noreply.github.com>
2023-10-04 01:33:07 +0000

    fix. cabeça e cor

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className= "text-center"> TESTE</h1>
+    <h1 className= "text-center mt-10"> TESTE</h1>
     
   )
 }
