345189ba99e16b4e2b25ff8df1ef2f94423aaf07
cleiton97 <146895519+cleiton97@users.noreply.github.com>
2023-11-07 23:40:23 +0000

    fet:Lista de Courses

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,10 @@
 export default function Admin(){
     return(
+        <div>
         <h1 className="text-center text-xl"> Area  da administrativa</h1>
+        <a href="/admin/course">Lista de cursos</a>
+
+        
+        </div>
     )
 }
