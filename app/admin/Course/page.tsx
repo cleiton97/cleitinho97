80892227bd6044091d38388092527bcf621a5cbb
cleiton97 <146895519+cleiton97@users.noreply.github.com>
2023-11-08 00:19:02 +0000

    fet: Lista de Courses

--- app/admin/Course/page.tsx
+++ app/admin/Course/page.tsx
@@ -1,9 +1,9 @@
 import Courses from "@/app/componetes/Courses";
-import { sql } from "@vercel/postgres";
+import { sql } from "@vercel/postgres"; 
 export const revalidate =0
 
 export default async function ListCourse(){
-    const { rows } = await sql 'SELECT * from courses';
+const { rows } = await sql `SELECT * from courses`;
     return(
         <div>
             <h1 className="text-center text-white"> Lista de Cursos</h1>
@@ -13,11 +13,12 @@ export default async function ListCourse(){
                 <tr> <td>Titulo do Curso</td> <td>Descrição</td></tr>
             </thead>
             <body>
+                
             {
                 rows.map((course) => {
                     return(
 
-            <tr key=(course.title)<td>({course,title}</td> <td>(course.description)</td>    
+            <tr key={course.id}><td>{course.title}</td> <td>{course.description}</td>    
             )
 
             })  
